872b18a684a59f01d37dfa549b95600ef80aecee
Dhinesh <dhinesh.babu@g7cr.com>
2026-03-16 15:35:02 +0530

    exploring routes

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ let port = 3000
 app.get('/',(req,res)=>{
     res.send('Hello Nodejs')
 })
+app.post('/',(req,res)=>{
+    res.send('posted succesfully')
+})
+app.patch('/',(req,res)=>{
+    res.send('update complete')
+})
+app.put('/',(req,res)=>{
+    res.send('replaced successfully')
+})
+app.delete('/',(req,res)=>{
+    res.send('delete complete')
+})
 app.listen(port,()=>{
     console.log(`server started on port ${port}`);
 })
